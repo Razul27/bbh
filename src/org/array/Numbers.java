316f09465ed3b78f4ec74a4a3e8b0195edce4c76
Razul27 <mohamedrazul916@gmail.com>
2022-07-23 08:35:13 +0530

    New Code

---
+++ src/org/array/Numbers.java
@@ -0,0 +1,17 @@
+package org.array;
+
+public class Numbers {
+	public static void main(String[] args) {
+		int i=0;
+		do {
+			System.out.println(i);
+			i++;
+		}while(i<10); {
+			System.out.println("***");
+		}
+	}
+	
+}
+
+
+
